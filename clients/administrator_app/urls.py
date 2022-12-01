e25521bb4e348f2a10a1cba851ef54d37d08d296
RudovichNazar23 <n.rudovich2005@gmail.com>
2022-12-01 16:51:51 +0100

    created a project

---
+++ clients/administrator_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+
+urlpatterns = [
+
+
+
+
+
+]
+
---
+++ clients/worker_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WorkerAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'worker_app'
