fcf56bc5b9fc75a32c551433387f7bd8b86ffc1b
RudovichNazar23 <n.rudovich2005@gmail.com>
2022-12-28 18:01:50 +0100

    added some styles fot reg_form

--- clients/client_app/views.py
+++ clients/client_app/views.py
@@ -20,7 +20,7 @@ class RegistrationView(View):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
-            password = form.cleaned_data.get("password")
+            password = form.cleaned_data.get("password1")
             user = authenticate(username=username, password=password)
             login(request, user)
             return redirect("/")
