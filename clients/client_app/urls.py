095f3567f651cdae48d361a50cbcf2585ece0415
Nazar <you@example.com>
2023-01-13 17:00:19 +0100

    added login view

--- clients/client_app/urls.py
+++ clients/client_app/urls.py
@@ -2,11 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.urls import path
-from .views import RegistrationView, MyProfileView
+from .views import RegistrationView, MyProfileView, LoginView
 
 urlpatterns = [
     path("registration", RegistrationView.as_view(), name="registration"),
     path("my_profile", MyProfileView.as_view(), name="my_profile"),
+    path("login", LoginView.as_view(), name="login")
 
 
 
