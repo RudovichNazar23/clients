27c9d050afb50cf96a5413461a4ed9f4f5dd4900
Nazar <you@example.com>
2023-02-27 17:02:23 +0100

    added schedule view

--- clients/worker_app/urls.py
+++ clients/worker_app/urls.py
@@ -3,12 +3,13 @@ from django.conf.urls.static import static
 
 
 from django.urls import path
-from .views import SignOutView
+from .views import SignOutView, MyScheduleView, WorkTimeOnDateView
 
 
 urlpatterns = [
-    path("logout", SignOutView.as_view(), name="logout")
-
+    path("logout", SignOutView.as_view(), name="logout"),
+    path("my_schedule", MyScheduleView.as_view(), name="my_schedule"),
+    path("my_schedule/time/<str:date>", WorkTimeOnDateView.as_view(), name="time")
 ]
 
 
