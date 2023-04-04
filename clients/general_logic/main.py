309b5ae3ac1e96073765c74bdab159b4e45a85aa
Nazar <you@example.com>
2023-04-04 18:38:24 +0200

    added some changes

--- clients/general_logic/main.py
+++ clients/general_logic/main.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
+from django.views import View
 
 
-def main(request):
-    if request.user.is_superuser:
-        return render(request, "administrator_app/main_admin.html")
-    elif request.user.is_staff:
-        return render(request, "worker_app/main_worker.html")
-    else:
-        return render(request, "client_app/home.html")
+class Main(View):
+    def get(self, request):
+        if request.user.is_superuser:
+            return render(request, "administrator_app/main_admin.html")
+        elif request.user.is_staff:
+            return render(request, "worker_app/main_worker.html")
+        else:
+            return render(request, "client_app/home.html")
 
