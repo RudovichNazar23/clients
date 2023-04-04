e400e00c7909ce880dd88cc57a3daf6fdaa929a2
Nazar <you@example.com>
2023-04-04 17:25:15 +0200

    added my_schedule_view

--- clients/worker_app/views.py
+++ clients/worker_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LogoutView
 from django.views import View
+from administrator_app.models import WorkDayAssignment
 
 
 class SignOutView(LogoutView):
@@ -8,3 +9,9 @@ class SignOutView(LogoutView):
     next_page = "/"
 
 
+class MyScheduleView(View):
+    template_name = "worker_app/schedule.html"
+
+    def get(self, request):
+        assignments = WorkDayAssignment.objects.filter(worker=request.user.id)
+        return render(request, self.template_name, {"assignments": assignments})
