26e7586f28eb3be590e9cf2fad9d20345b92f139
Nazar <you@example.com>
2023-04-10 15:51:29 +0200

    changed styles in all apps

--- clients/clients/settings.py
+++ clients/clients/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'clients.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
+        'DIRS': [ BASE_DIR, "templates"
         ],
         'APP_DIRS': True,
         'OPTIONS': {
