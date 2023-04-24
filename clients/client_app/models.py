341e10320d0f185d7a6406921e552ad219be7ed1
Nazar <you@example.com>
2023-04-24 17:23:12 +0200

    added time_choices for order_service_form

--- clients/client_app/models.py
+++ clients/client_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from worker_app.models import Worker
 from administrator_app.models import Service, WorkDayAssignment
+from django.utils.deconstruct import deconstructible
 
 
 class NotRegisteredUser(models.Model):
@@ -13,12 +14,38 @@ class NotRegisteredUser(models.Model):
     def __str__(self):
         return f"{self.first_name}"
 
+@deconstructible
+class Time(models.Model):
+    def __init__(self, time: str, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.time = time
+
+    ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.time}"
+
+
+TIME_CHOICES = [
+    ("9:00", Time("9:00")),
+    ("9:45", Time("9:45")),
+    ("10:30", Time("10:30")),
+    ("11:15", Time("11:15")),
+    ("12:00", Time("12:00")),
+    ("12:45", Time("12:45")),
+    ("13:30", Time("13:30")),
+    ("14:15", Time("14:15")),
+    ("15:00", Time("15:00")),
+    ("15:45", Time("15:45")),
+    ("16:30", Time("16:30")),
+]
+
 
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     worker_and_date = models.ForeignKey(WorkDayAssignment, on_delete=models.CASCADE)
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
-    time = models.CharField(verbose_name="Time of service", max_length=100)
+    time = models.CharField(verbose_name="Time of service", max_length=100, choices=TIME_CHOICES)
     active = models.BooleanField(default=True)
 
     def __str__(self):
