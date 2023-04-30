e02900e00e2a6593c1c1b3569aea00257eb39c1d
Nazar <you@example.com>
2023-04-30 19:08:18 +0200

    created view for showing client's visits

--- clients/client_app/urls.py
+++ clients/client_app/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.urls import path
-from .views import RegistrationView, MyProfileView, LoginView, SignOutView, MainPageView, WorkerProfileView, ServiceProfileView, OrderServiceView
+from .views import RegistrationView, MyProfileView, LoginView, SignOutView, MainPageView, WorkerProfileView, ServiceProfileView, OrderServiceView, MyVisitsView
 
 
 urlpatterns = [
@@ -13,7 +13,8 @@ urlpatterns = [
     path("main", MainPageView.as_view(), name="main"),
     path("worker_info/<str:first_name>", WorkerProfileView.as_view(), name="worker_info"),
     path("service_info/<str:name>", ServiceProfileView.as_view(), name="service_info"),
-    path("order_service", OrderServiceView.as_view(), name="order_service")
+    path("order_service", OrderServiceView.as_view(), name="order_service"),
+    path("my_visits", MyVisitsView.as_view(), name="my_visits")
 ]
 
 if settings.DEBUG:
